2c4ebfdca779abf3a8f851abff2e86d4f6e23bad
OpenShift guest <mh223ci@student.lnu.se>
2013-11-19 09:50:44 -0500

    strings klart

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -6,12 +6,56 @@ window.onload = function(){
 	var convertString = function(str){
 		// Plats för förändring.	
 		
+		// En tom sträng vi returnerar den konverterade strängen till.
+		var letterString = "";
 		
+		// Räknar igenom strängen bokstav för bokstav.
 		
-		// Returnera den konverterade strängen.
+		for(var i = 0; i < str.length; i += 1){
+		var upperCaseLetter = str.charAt(i);
+		var lowerCaseLetter = str.charAt(i);
+		
+		// Kollar om bokstaven är stor eller liten. 
+		
+		upperCaseLetter = upperCaseLetter == str.charAt(i).toUpperCase();
+		lowerCaseLetter = lowerCaseLetter == str.charAt(i).toLowerCase();
+		
+		// Kollar om bokstaven är ett a || A och byter ut de med #.
+		
+		if(str.charAt(i) === "a" || str.charAt(i) === "A"){
+		    letterString += "#";
+		}
+		
+		else
+		{
+		    // Om det är en stor bokstav görs den om till en liten.
+		    
+		if(upperCaseLetter == true){
+		    letterString += str.charAt(i).toLowerCase();
+		}
+		
+		// Om det är en liten bokstav görs den om till en stor.
+		
+		if(lowerCaseLetter == true){
+		    letterString += str.charAt(i).toUpperCase();
+		}
+		
+	    }
+	        
+		}
+		
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
 	
-
+        if (!isNaN(str * 1)){
+            return["FEL! Måste mata in bokstäver."]
+        }
+		
+		// Returnera den konverterade strängen.
+		
+		return letterString;
+		
+		
+        
 
 
 
